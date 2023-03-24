22dd3f14aab33ddeb542daa110de3a25ad55201c
Spencer Williams <srwilliams349@gmail.com>
2023-03-24 19:35:54 -0400

    starter code

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,19 @@
+package com.skilldistillery.foodtruck.app;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
+public class FoodTruckApp {
+	private FoodTruck[] fleet;
+	//only class with scanner. none in FoodTruck.
+	
+	
+	public static void main(String[] args) {
+		FoodTruckApp fta = new FoodTruckApp();
+		fta.run();
+	}
+
+	public void run() {
+	}
+	
+	
+}
---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class FoodTruck {
+	private String name;
+	
+	
+	// ctors 
+	
+	
+	// getters and setters
+	
+	
+	// toString()
+}
